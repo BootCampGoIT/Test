99a10516e272b63628f2fc0a849a59d5366feae3
AndriiShnurkov <Shans12.84@ukr.net>
2020-05-12 12:33:28 +0300

    changed gitignore

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { v4: uuidv4 } = require('uuid');
-const shortid = require('shortid');
+import {v4 as uuidv4} from 'uuidv4';
+// const shortid = require('shortid');
 
 console.log(uuidv4());
-console.log(shortid.generate());
+// console.log(shortid.generate());
